1c8d26a58754212449ba9b0d0b023b6b478fdd62
Henry Harder <henry@paradigm.market>
2019-04-10 09:05:27 -0700

    update golang test project

--- packages/go-test/animal/animal.go
+++ packages/go-test/animal/animal.go
@@ -4,22 +4,21 @@ import (
 	"errors"
 )
 
-var (
-	allowed = map[string]bool{
-		"cow":   true,
-		"horse": true,
-		"cat":   true,
-		"dog":   true,
-		"bird":  true,
-	}
-)
+// Explicitly only allow these types of Animals to be created
+var allowed = map[string]bool{
+	"cow":   true,
+	"horse": true,
+	"cat":   true,
+	"dog":   true,
+	"bird":  true,
+}
 
 // Animal represents a simple animal
 type Animal struct {
-	name    string
-	species string
-	age     uint
-	alive   bool
+	Name    string
+	Species string
+	Age     uint
+	Alive   bool
 }
 
 // NewAnimal creates a new animal instance with specified name and type
@@ -28,51 +27,51 @@ func NewAnimal(species string, name string) (*Animal, error) {
 		return nil, errors.New("invalid species")
 	}
 	return &Animal{
-		name:    name,
-		age:     0,
-		species: species,
-		alive:   true,
+		Name:    name,
+		Age:     0,
+		Species: species,
+		Alive:   true,
 	}, nil
 }
 
 // SayName returns the animals name
-func (a *Animal) SayName() (name string) {
-	name = a.name
-	return name
+func (a Animal) SayName() (name string) {
+	name = a.Name
+	return
 }
 
 // SaySpecies returns the animals species
-func (a *Animal) SaySpecies() (species string) {
-	species = a.species
-	return species
+func (a Animal) SaySpecies() (species string) {
+	species = a.Species
+	return
 }
 
 // IsAlive returns true if the animal is alive
-func (a *Animal) IsAlive() (living bool) {
-	living = a.alive
-	return living
+func (a Animal) IsAlive() (living bool) {
+	living = a.Alive
+	return
 }
 
 // HowOld returns the animals age
-func (a *Animal) HowOld() (age uint) {
-	age = a.age
-	return age
+func (a Animal) HowOld() (age uint) {
+	age = a.Age
+	return
 }
 
 // Grow increases the animals age by one
 func (a *Animal) Grow() (err error) {
-	if a.alive != true {
+	if a.Alive != true {
 		return errors.New("animal that is dead cannot grow")
 	}
-	a.age++
+	a.Age++
 	return nil
 }
 
 // Kill kills an animal
 func (a *Animal) Kill() (err error) {
-	if a.alive != true {
+	if a.Alive != true {
 		return errors.New("cannot kill what is already dead")
 	}
-	a.alive = false
+	a.Alive = false
 	return nil
 }
