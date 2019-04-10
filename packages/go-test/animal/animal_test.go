bb53b7076c2ad70e4707e2ff115fcc155cd919a2
Henry Harder <henry@paradigm.market>
2019-04-09 21:02:10 -0700

    adding tests to mirror ts example

--- packages/go-test/animal/animal_test.go
+++ packages/go-test/animal/animal_test.go
@@ -1,25 +1,95 @@
 package animal
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestNewAnimal(t *testing.T) {
-	if _, err := NewAnimal("horace", "cow"); err != nil {
-		t.Fatalf("should have allowed creation of cow")
+	if _, err := NewAnimal("dog", "richard"); err != nil {
+		t.Fatalf("should have allowed creation of dog")
 	}
 
-	if _, err := NewAnimal("horace", "horse"); err != nil {
-		t.Fatalf("should have allowed creation of horse")
-	}
-
-	if _, err := NewAnimal("horace", "cat"); err != nil {
+	if _, err := NewAnimal("cat", "richard"); err != nil {
 		t.Fatalf("should have allowed creation of cat")
 	}
 
-	if _, err := NewAnimal("horace", "dog"); err == nil {
-		t.Fatalf("should not have allowed creation of dog")
+	if _, err := NewAnimal("cow", "richard"); err != nil {
+		t.Fatalf("should have allowed creation of cow")
 	}
 
-	if _, err := NewAnimal("horace", "bird"); err == nil {
-		t.Fatalf("should not have allowed creation of bird")
+	if _, err := NewAnimal("bird", "richard"); err != nil {
+		t.Fatalf("should have allowed creation of bird")
 	}
+
+	if _, err := NewAnimal("lizard", "richard"); err == nil {
+		t.Fatalf("should not have allowed creation of lizard")
+	}
+}
+
+func TestAnimalSaysName(t *testing.T) {
+	name := "richard"
+	species := "dog"
+
+	dog, err := NewAnimal(species, name)
+	require.Nil(t, err, "should have allowed creation of richard")
+
+	saidName := dog.SayName()
+	require.Equal(t, name, saidName)
+}
+
+func TestAnimalSaysSpecies(t *testing.T) {
+	name := "richard"
+	species := "dog"
+
+	richard, err := NewAnimal(species, name)
+	require.Nil(t, err, "should have allowed creation of richard")
+
+	saidSpecies := richard.SaySpecies()
+	require.Equal(t, species, saidSpecies)
+}
+
+func TestAnimalIsAlive(t *testing.T) {
+	richard, err := NewAnimal("dog", "richard")
+	require.Nil(t, err, "should have allowed creation of richard")
+
+	isAlive := richard.IsAlive()
+	require.Equal(t, true, isAlive, "richard should have been alive")
+}
+
+func TestAnimalAge(t *testing.T) {
+	richard, err := NewAnimal("dog", "richard")
+	require.Nil(t, err, "should have allowed creation of richard")
+
+	initialAge := richard.HowOld()
+	require.Equal(t, uint(0), initialAge, "richards age should have been 0")
+
+	// Test that richard ages one year each time we .Grow him
+	err = richard.Grow()
+	require.Nil(t, err, "should have been able to grow richard")
+
+	newAge := richard.HowOld()
+	require.Equal(t, uint(1), newAge, "richards age should have been 1")
+}
+
+func TestKillAnimal(t *testing.T) {
+	richard, err := NewAnimal("dog", "richard")
+	require.Nil(t, err, "should have allowed creation of richard")
+
+	isAlive := richard.IsAlive()
+	require.Equal(t, true, isAlive, "richard should have been alive")
+
+	// We must kill Richard :(
+	err = richard.Kill()
+	require.Nil(t, err, "should have been able to kill richard")
+
+	isAliveNow := richard.IsAlive()
+	require.Equal(t, false, isAliveNow, "richard should have been dead now")
+
+	err = richard.Kill()
+	require.NotNil(t, err, "should not have been able to kill richard again")
+
+	err = richard.Grow()
+	require.NotNil(t, err, "should not have been able to grow dead richard")
 }
--- packages/go-test/go.mod
+++ packages/go-test/go.mod
@@ -1,3 +1,5 @@
 module go-test
 
 go 1.12
+
+require github.com/stretchr/testify v1.3.0
