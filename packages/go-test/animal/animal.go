546b44d61313126c058c7c1c3a40bbacd36412d1
Henry Harder <henry@paradigm.market>
2019-04-09 21:04:02 -0700

    adding new animal implementation

--- packages/go-test/animal/animal.go
+++ packages/go-test/animal/animal.go
@@ -4,25 +4,75 @@ import (
 	"errors"
 )
 
-// Animal is an animal
-type Animal struct {
-	Name string
-	Age  uint
-	Type string
-}
-
-// NewAnimal creates a new animal
-func NewAnimal(name string, species string) (*Animal, error) {
-	types := map[string]bool{
+var (
+	allowed = map[string]bool{
 		"cow":   true,
 		"horse": true,
 		"cat":   true,
-		"dog":   false,
-		"bird":  false,
+		"dog":   true,
+		"bird":  true,
 	}
+)
 
-	if !types[species] {
+// Animal represents a simple animal
+type Animal struct {
+	name    string
+	species string
+	age     uint
+	alive   bool
+}
+
+// NewAnimal creates a new animal instance with specified name and type
+func NewAnimal(species string, name string) (*Animal, error) {
+	if !allowed[species] {
 		return nil, errors.New("invalid species")
 	}
-	return &Animal{Name: name, Age: 0, Type: species}, nil
+	return &Animal{
+		name:    name,
+		age:     0,
+		species: species,
+		alive:   true,
+	}, nil
+}
+
+// SayName returns the animals name
+func (a *Animal) SayName() (name string) {
+	name = a.name
+	return name
+}
+
+// SaySpecies returns the animals species
+func (a *Animal) SaySpecies() (species string) {
+	species = a.species
+	return species
+}
+
+// IsAlive returns true if the animal is alive
+func (a *Animal) IsAlive() (living bool) {
+	living = a.alive
+	return living
+}
+
+// HowOld returns the animals age
+func (a *Animal) HowOld() (age uint) {
+	age = a.age
+	return age
+}
+
+// Grow increases the animals age by one
+func (a *Animal) Grow() (err error) {
+	if a.alive != true {
+		return errors.New("animal that is dead cannot grow")
+	}
+	a.age++
+	return nil
+}
+
+// Kill kills an animal
+func (a *Animal) Kill() (err error) {
+	if a.alive != true {
+		return errors.New("cannot kill what is already dead")
+	}
+	a.alive = false
+	return nil
 }
