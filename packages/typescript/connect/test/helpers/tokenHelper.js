2fa0a68ed2843abc4d286cda6679602ff533d894
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-04-24 10:05:29 -0500

    Removing simple-erc20 wraping contract methods

--- packages/typescript/connect/test/helpers/tokenHelper.js
+++ packages/typescript/connect/test/helpers/tokenHelper.js
@@ -1,5 +1,44 @@
 const TokenContractInfo = require('./TestToken.json');
-const SimpleERC20 = require('simple-erc20');
+
+function wrap(contract) {
+  const methods = contract.methods;
+
+  return {
+    name: function name() {
+      return methods.name().call();
+    },
+    symbol: function symbol() {
+      return methods.symbol().call();
+    },
+    totalSupply: function totalSupply() {
+      return methods.totalSupply().call();
+    },
+    decimals: function decimals() {
+      return methods.decimals().call();
+    },
+    balanceOf: function balanceOf(owner) {
+      return methods.balanceOf(owner).call();
+    },
+    allowance: function allowance(owner, spender) {
+      return methods.allowance(owner, spender).call();
+    },
+    approve: async function approve(spender, value, from = null) {
+      if (from === null) from = await getCoinbase();
+      var tx = methods.approve(spender, value);
+      return tx.send({ from: from });
+    },
+    transferFrom: async function transferFrom(fromAddress, to, value, from = null) {
+      if (from === null) from = await getCoinbase();
+      var tx = methods.transferFrom(fromAddress, to, value);
+      return tx.send({ from: from });
+    },
+    transfer: async function transfer(to, value, from = null) {
+      if (from === null) from = await getCoinbase();
+      var tx = methods.transfer(to, value);
+      return tx.send({ from: from });
+    }
+  }
+};
 
 module.exports = async () => {
   const TokenContract = new web3.eth.Contract(TokenContractInfo.abi);
@@ -8,8 +47,8 @@ module.exports = async () => {
   const tkbContract = await TokenContract.deploy({ data: TokenContractInfo.bytecode , arguments: ['Token B', 'TKB'] }).send({ from: accounts[8], gas: 4500000 });
 
   global.TKA = tkaContract.options.address;
-  global.tka = SimpleERC20(TKA, await web3.eth.net.getId(), web3);
+  global.tka = wrap(tkaContract);
 
   global.TKB = tkbContract.options.address;
-  global.tkb = SimpleERC20(TKB, await web3.eth.net.getId(), web3);
+  global.tkb = wrap(tkbContract);
 };
