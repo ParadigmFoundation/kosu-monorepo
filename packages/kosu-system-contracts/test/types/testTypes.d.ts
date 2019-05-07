bc2ca880074a40b062a2cb437c80605f42660316
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-05-07 17:16:48 -0500

    Exposing addtional testing values and globals

--- packages/kosu-system-contracts/test/types/testTypes.d.ts
+++ packages/kosu-system-contracts/test/types/testTypes.d.ts
@@ -1,14 +1,17 @@
 interface TestValues {
-    maxUint: BN;
-    sixEther: BN;
-    fiveEther: BN;
-    oneEther: BN;
-    halfEther: BN;
-    oneHundredWei: BN;
-    fiftyWei: BN;
+    maxUint: BigNumber;
+    sixEther: BigNumber;
+    fiveEther: BigNumber;
+    oneEther: BigNumber;
+    halfEther: BigNumber;
+    oneHundredWei: BigNumber;
+    fiftyWei: BigNumber;
+    zero: BigNumber;
 }
 
 declare const testValues: TestValues;
 declare const accounts: string[];
 declare const contracts: MigratedContracts;
 declare const web3Wrapper: Web3Wrapper;
+declare const skipBlocks: any;
+declare const txDefaults: any;
