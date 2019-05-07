1f46074670d08379a765d3e1a0baa992f4001d85
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-05-07 09:22:07 -0500

    fixing linter issues

--- packages/kosu-system-contracts/src/types.d.ts
+++ packages/kosu-system-contracts/src/types.d.ts
@@ -1,13 +1,13 @@
 
 interface MigratedContracts {
-    validatorRegistryProxy: ValidatorRegistryProxyContract,
-    orderGateway: OrderGatewayContract,
-    eventEmitter: EventEmitterContract,
-    treasury: TreasuryContract,
-    authorizedAddresses: AuthorizedAddressesContract,
-    kosuToken: KosuTokenContract,
-    validatorRegistryImpl: ValidatorRegistryContract,
-    voting: VotingContract,
-    posterRegistryProxy: PosterRegistryProxyContract,
-    posterRegistryImpl: PosterRegistryContract
+    validatorRegistryProxy: ValidatorRegistryProxyContract;
+    orderGateway: OrderGatewayContract;
+    eventEmitter: EventEmitterContract;
+    treasury: TreasuryContract;
+    authorizedAddresses: AuthorizedAddressesContract;
+    kosuToken: KosuTokenContract;
+    validatorRegistryImpl: ValidatorRegistryContract;
+    voting: VotingContract;
+    posterRegistryProxy: PosterRegistryProxyContract;
+    posterRegistryImpl: PosterRegistryContract;
 }
