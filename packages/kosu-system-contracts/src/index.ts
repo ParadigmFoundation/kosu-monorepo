4be4a720c8c7e64a14668b17bfd05c88c25c61bd
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-05-09 09:56:00 -0500

    Exposing the deployed addresses

--- packages/kosu-system-contracts/src/index.ts
+++ packages/kosu-system-contracts/src/index.ts
@@ -1,3 +1,6 @@
+import * as deployedAddresses from "./deployedAddresses.json";
+
+export const DeployedAddresses = deployedAddresses;
 export * from "./eventDecoder";
 export * from "./artifacts";
 export * from "./wrappers";
