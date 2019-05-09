abdb0a958a0ba71d5ac2b8630f4bc5ee1b73d6f8
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-05-09 14:11:35 -0500

    exposing listing decoder

--- packages/kosu-system-contracts/src/index.ts
+++ packages/kosu-system-contracts/src/index.ts
@@ -2,5 +2,6 @@ import * as deployedAddresses from "./deployedAddresses.json";
 
 export const DeployedAddresses = deployedAddresses;
 export * from "./eventDecoder";
+export * from "./listingDecoder";
 export * from "./artifacts";
 export * from "./wrappers";
