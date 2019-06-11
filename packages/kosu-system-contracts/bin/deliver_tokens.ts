5336ce973318bdbd62e3f86b8f09b0dceab9ca63
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-06-11 09:37:12 -0500

    updating deliver_tokens to also give kosu node addresses tokens

--- packages/kosu-system-contracts/bin/deliver_tokens.ts
+++ packages/kosu-system-contracts/bin/deliver_tokens.ts
@@ -1,7 +1,7 @@
 import { MnemonicWalletSubprovider, RPCSubprovider } from "@0x/subproviders";
 import { BigNumber, providerUtils } from "@0x/utils";
-import Web3 from "web3";
 import safeRequire from "safe-node-require";
+import Web3 from "web3";
 import Web3ProviderEngine from "web3-provider-engine";
 import yargs from "yargs";
 
@@ -37,7 +37,11 @@ if (args["test-mnemonic"] || !mnemonic) {
     );
     const aLot = new BigNumber(web3.utils.toWei("100000"));
     const from = await web3.eth.getCoinbase();
+    const addresses = ["0xAA554D0c5ff879387Fc234dE5D22EC02983baA27", "0x8b366a3d4e46aC5406F12766Ad33E6482Ce4F081"];
+    addresses.push(await web3.eth.getAccounts());
 
-    await kosuToken.transfer.awaitTransactionSuccessAsync("0xAA554D0c5ff879387Fc234dE5D22EC02983baA27", aLot, { from });
-    await kosuToken.transfer.awaitTransactionSuccessAsync("0x8b366a3d4e46aC5406F12766Ad33E6482Ce4F081", aLot, { from });
+    for (const account of addresses) {
+        await kosuToken.transfer.awaitTransactionSuccessAsync(account, aLot, { from });
+    }
+
 })().catch(err => console.log(err));
