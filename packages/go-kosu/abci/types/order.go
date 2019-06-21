bb8e9640d898646ad78c49f89098c6991f9ca5e7
Henry Harder <henry@paradigm.market>
2019-06-21 00:23:07 -0700

    passing test cases

--- packages/go-kosu/abci/types/order.go
+++ packages/go-kosu/abci/types/order.go
@@ -1,16 +1,17 @@
 package types
 
 import (
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
-type Values map[string]interface{}
-
-func (v *Values) String() (values string) {
-	return "< ... >"
-}
-
+// Order represents a Kosu order object for use with a SubContract
 type Order struct {
 	Maker           Address   `json:"maker"`
 	SubContract     Address   `json:"subContract"`
@@ -18,14 +19,6 @@ type Order struct {
 	MakerValues     Values    `json:"makerValues"`
 	MakerSignature  Signature `json:"makerSignature"`
 	PosterSignature Signature `json:"posterSignature"`
-}
-
-func (o *Order) String() (order string) {
-	return fmt.Sprintf("<%v>", o.MakerValues)
-}
-
-func (o *Order) PosterHex() (hex [32]byte, e error) {
-	return
 }
 
 // NewOrder creates a new Kosu Order object from a JSON string
@@ -38,12 +31,56 @@ func NewOrder(input string) (order *Order, e error) {
 	return
 }
 
+// PosterHex generates the hash used to sign the order
+func (o *Order) PosterHex() (posterHex []byte, e error) {
+	orderBytes, err := o.Serialize()
+	if err != nil {
+		e = errors.New("Failed to serialize order")
+		return
+	}
+
+	posterHex = crypto.Keccak256(orderBytes)
+	return
+}
+
+// Serialize generates a byte array of the MakerValues as expected by Kosu contract system
+func (o *Order) Serialize() (orderBytes []byte, e error) {
+	for _, m := range o.Arguments.Maker {
+		switch m.DataType {
+		case "address":
+			bytes, err := hex.DecodeString(o.MakerValues[m.Name].(string)[2:])
+			if err != nil {
+				return orderBytes, err
+			}
+			orderBytes = append(orderBytes, bytes...)
+		case "uint":
+			bigFloat := big.NewFloat(o.MakerValues[m.Name].(float64))
+			intVal, _ := bigFloat.Int64()
+			slice := abi.U256(big.NewInt(intVal))
+			orderBytes = append(orderBytes, slice...)
+		case "signature":
+			slice := []byte(o.MakerValues[m.Name].(string)[2:])
+			orderBytes = append(orderBytes, slice...)
+		}
+	}
+	return
+}
+
+// String implements Stringer to represent Order as a string
+func (o *Order) String() (order string) {
+	return fmt.Sprintf("<%v>", "@todo")
+}
+
+// Arguments defines the data types required for maker and taker values
 type Arguments struct {
 	Maker []Argument `json:"maker"`
 	Taker []Argument `json:"taler"`
 }
 
+// Argument defines a maker or taker value with parameter name and data type
 type Argument struct {
 	DataType string `json:"dataType"`
 	Name     string `json:"name"`
 }
+
+type Values map[string]interface{}
--- packages/go-kosu/abci/types/order_test.go
+++ packages/go-kosu/abci/types/order_test.go
@@ -1,34 +1,27 @@
 package types
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 var testCases = []struct {
-	desc  string
-	order string
-	hex   string
-	bytes [32]byte
+	desc      string
+	order     string
+	hex       string
+	hash      string
+	hexBytes  []byte
+	hashBytes []byte
 }{
 	{
-		desc:  "Mocker order #1",
-		order: `{"subContract":"0x63b577d6cc0f39d329f7bf1a4bccc4a1eafc8292","maker":"0xcb518d697c99d0d243e3c73fb5dea8464bddd2da","arguments":{"maker":[{"datatype":"address","name":"signer"},{"datatype":"address","name":"signerToken"},{"datatype":"uint","name":"signerTokenCount"},{"datatype":"address","name":"buyerToken"},{"datatype":"uint","name":"buyerTokenCount"},{"datatype":"signature","name":"signature","signatureFields":[0,1,2,3,4]}],"taker":[{"datatype":"uint","name":"tokensToBuy"}]},"makerValues":{"signer":"0xcb518d697c99d0d243e3c73fb5dea8464bddd2da","signerToken":"0x56f34847BEDE8Ed728b38E68DDeB6758bEBBbBde","signerTokenCount":1000,"buyer":"0x4d81cb2e8dd024cae8a1ea86c0d50be681cb4e0e","buyerToken":"0x011ac35d0f3218058A155f3e21D005B6089d0Fa3","buyerTokenCount":1000,"signature":"0xdee0968e41b912d93f8ee5eb5a89f69b93c6c1fff88d3b1ccb452ae5a08342361dfbc5a4afa9829bfb64294321f8c8d0bacf60e5dda81f64a82d004d42570b4500"},"makerSignature":"0xdee0968e41b912d93f8ee5eb5a89f69b93c6c1fff88d3b1ccb452ae5a08342361dfbc5a4afa9829bfb64294321f8c8d0bacf60e5dda81f64a82d004d42570b4500","takerValues":{"tokensToBuy":100},"posterSignature":"0xf5c94d06b88c5a6341cf008b62593fdb322b2c750f49091d0fa5e4703127bbce3e3588f4b04c13881ee1bc9b28e18666252e8531f46e67201aa0b714601253ca00"}`,
-		hex:   "0x4811f841441b2d7e0516b46d4a7b77caa1a316a39b8358318848e3ef7c6db3ef",
-		bytes: [32]byte{83, 171, 177, 22, 75, 73, 248, 39, 69, 60, 99, 165, 119, 191, 167, 15, 52, 180, 202, 245, 161, 189, 3, 187, 13, 153, 144, 181, 95, 70, 246, 104},
-	},
-	{
-		desc:  "Mocker order #2",
-		order: `{"subContract":"0x1a182389482c411f2149ae6b561c10bdb45566df","maker":"0x1c4518bc8dcd0824b7d1cd87112806d96fdc9e7a","arguments":{"maker":[{"datatype":"address","name":"signer"},{"datatype":"address","name":"signerToken"},{"datatype":"uint","name":"signerTokenCount"},{"datatype":"address","name":"buyerToken"},{"datatype":"uint","name":"buyerTokenCount"},{"datatype":"signature","name":"signature","signatureFields":[0,1,2,3,4]}],"taker":[{"datatype":"uint","name":"tokensToBuy"}]},"makerValues":{"signer":"0x1c4518bc8dcd0824b7d1cd87112806d96fdc9e7a","signerToken":"0x323d158Dd8903db645B378FeBfA8653B1912D6a1","signerTokenCount":1000,"buyer":"0x23e556b2ea0a696e533d92f0209b9460ffbc1c1a","buyerToken":"0x70A38470B7736962f4B6207D521c558B3261C426","buyerTokenCount":1000,"signature":"0xfc860d67b460c013b119c8ab8ae3095dead0b6c7d261f066bae76f3d591e43e87b098d492f8468fe8c22e10b700127d43593d6954ecdb96a5cd5f94df5b2943b00"},"makerSignature":"0xfc860d67b460c013b119c8ab8ae3095dead0b6c7d261f066bae76f3d591e43e87b098d492f8468fe8c22e10b700127d43593d6954ecdb96a5cd5f94df5b2943b00","takerValues":{"tokensToBuy":100}}`,
-		hex:   "0x8ffbd8b78588cea15d2f48de5018b9c6f634ba7df279efef4ecb2682e68b55f8",
-		bytes: [32]byte{83, 171, 177, 22, 75, 73, 248, 39, 69, 60, 99, 165, 119, 191, 167, 15, 52, 180, 202, 245, 161, 189, 3, 187, 13, 153, 144, 181, 95, 70, 246, 104},
-	},
-	{
-		desc:  "Mocker order #3",
-		order: `{"subContract":"0x23df63a15ab38ae44930931ad46307a264494c09","maker":"0x09b64c1eac150d4fb938a26ffdbdde4b9a91c2c5","arguments":{"maker":[{"datatype":"address","name":"signer"},{"datatype":"address","name":"signerToken"},{"datatype":"uint","name":"signerTokenCount"},{"datatype":"address","name":"buyerToken"},{"datatype":"uint","name":"buyerTokenCount"},{"datatype":"signature","name":"signature","signatureFields":[0,1,2,3,4]}],"taker":[{"datatype":"uint","name":"tokensToBuy"}]},"makerValues":{"signer":"0x09b64c1eac150d4fb938a26ffdbdde4b9a91c2c5","signerToken":"0x5EAAE17530F327261639c3e84A67Eb2992c2e7B0","signerTokenCount":1000,"buyer":"0xe6549d86624a3c371ad6910c159e1f79f8509fad","buyerToken":"0x662EfBf0E42bf2DB7FD2ae5245Dff12BC56498aF","buyerTokenCount":1000,"signature":"0x87831877e11d00a87508d4ff3c861a85aae2069a94de48e1956ac0f8ac150b1d5bef3d575f391e70110dfccbe40f6ba8c90b3695c6f29bdaf8b1f01c955344f600"},"makerSignature":"0x87831877e11d00a87508d4ff3c861a85aae2069a94de48e1956ac0f8ac150b1d5bef3d575f391e70110dfccbe40f6ba8c90b3695c6f29bdaf8b1f01c955344f600","takerValues":{"tokensToBuy":100}}`,
-		hex:   "0x53abb1164b49f827453c63a577bfa70f34b4caf5a1bd03bb0d9990b55f46f668",
-		bytes: [32]byte{83, 171, 177, 22, 75, 73, 248, 39, 69, 60, 99, 165, 119, 191, 167, 15, 52, 180, 202, 245, 161, 189, 3, 187, 13, 153, 144, 181, 95, 70, 246, 104},
+		desc:      "Mocker order #1",
+		order:     `{"subContract":"0x0000000000000000000000000000000000000000","arguments":{"maker":[{"datatype":"address","name":"acc"},{"datatype":"uint","name":"num"}]},"makerValues":{"acc":"0xba71358156bbf446A9Ec3126b818fe8fA1Dcd580","num":4},"posterSignature":"0xafbb347be9abed3004d002eeafa7cac1eb9924b53627e0bc94068f0f304cef1806a9e0ce2187228baf2b06926caab3e130b22d5781878a08f2bc4b75e6db20b301"}`,
+		hex:       "0xba71358156bbf446A9Ec3126b818fe8fA1Dcd5800000000000000000000000000000000000000000000000000000000000000004",
+		hash:      "0xa5d262bb0c7d17f8f4b3d0fd12fc9845ee390b0f95a965a62a1a67b7db04e2be",
+		hexBytes:  []byte{186, 113, 53, 129, 86, 187, 244, 70, 169, 236, 49, 38, 184, 24, 254, 143, 161, 220, 213, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+		hashBytes: []byte{165, 210, 98, 187, 12, 125, 23, 248, 244, 179, 208, 253, 18, 252, 152, 69, 238, 57, 11, 15, 149, 169, 101, 166, 42, 26, 103, 183, 219, 4, 226, 190},
 	},
 }
 
@@ -45,13 +38,24 @@ func TestCreateOrder(t *testing.T) {
 func TestPosterHex(t *testing.T) {
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
+			// sanity checks
+			hexBytesComp, _ := hex.DecodeString(tC.hex[2:])
+			require.EqualValues(t, tC.hexBytes, hexBytesComp)
+
+			hashBytesComp, _ := hex.DecodeString(tC.hash[2:])
+			require.EqualValues(t, tC.hashBytes, hashBytesComp)
+
+			// test order
 			order, err := NewOrder(tC.order)
 			require.NoError(t, err, "Expected no error creating order")
 
-			hex, err := order.PosterHex()
-			require.NoError(t, err, "Expected no error generating poster hex")
+			orderBytes, err := order.Serialize()
+			require.NoError(t, err, "Expected no error generating order bytes")
+			require.EqualValues(t, tC.hexBytes, orderBytes, "Expected order bytes to match test case")
 
-			require.EqualValues(t, tC.bytes, hex, "Expected hex bytes to match")
+			orderHash, err := order.PosterHex()
+			require.NoError(t, err, "Expected no error generating poster hash")
+			require.EqualValues(t, tC.hashBytes, orderHash, "Expected hex bytes to match")
 		})
 	}
 }
