0ce65596f4c31687d402bedbdd5f838b7a78ea9b
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-06-23 23:30:07 -0500

    Running prettier.

--- packages/kosu.js/test/voting_test.js
+++ packages/kosu.js/test/voting_test.js
@@ -7,9 +7,21 @@ describe("Voting", () => {
         const pollId = await testHelpers.variablePoll(3, 1);
         await kosu.voting.commitVote(pollId, encodedVote, TestValues.oneWei).should.be.fulfilled;
         await kosu.voting.revealVote(pollId, voteValue, voteSalt);
-        await kosu.voting.winningOption(pollId).then(x => x.toString()).should.eventually.eq(voteValue.toString());
-        await kosu.voting.totalWinningTokens(pollId).then(x => x.toString()).should.eventually.eq(voteValue.toString());
-        await kosu.voting.totalRevealedTokens(pollId).then(x => x.toString()).should.eventually.eq(voteValue.toString());
-        await kosu.voting.userWinningTokens(pollId).then(x => x.toString()).should.eventually.eq(voteValue.toString());
+        await kosu.voting
+            .winningOption(pollId)
+            .then(x => x.toString())
+            .should.eventually.eq(voteValue.toString());
+        await kosu.voting
+            .totalWinningTokens(pollId)
+            .then(x => x.toString())
+            .should.eventually.eq(voteValue.toString());
+        await kosu.voting
+            .totalRevealedTokens(pollId)
+            .then(x => x.toString())
+            .should.eventually.eq(voteValue.toString());
+        await kosu.voting
+            .userWinningTokens(pollId)
+            .then(x => x.toString())
+            .should.eventually.eq(voteValue.toString());
     });
 });
