922d0cd468e6cf0b2035c42f3a2862f461c3c9bb
Gustavo Chain <gchain@pm.me>
2019-07-11 21:56:23 +0200

    go-kosu: fix broken abci test

--- packages/go-kosu/abci/witness_test.go
+++ packages/go-kosu/abci/witness_test.go
@@ -2,10 +2,12 @@ package abci
 
 import (
 	"go-kosu/abci/types"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/db"
 )
 
@@ -14,11 +16,16 @@ func TestDeliverWitnessTx(t *testing.T) {
 	done, app := newTestApp(t, db)
 	defer done()
 
+	app.currentValidators = []abci.Validator{
+		{Address: []byte{0xff, 0xff, 0xff, 0xff}, Power: 1},
+	}
+
 	_, priv, err := types.NewKeyPair()
 	require.NoError(t, err)
 
 	wtx := &types.TransactionWitness{
-		Block: 1,
+		Block:   1,
+		Address: "ffffffff",
 	}
 
 	tx, err := types.WrapTx(wtx).SignedTransaction(priv)
@@ -29,5 +36,6 @@ func TestDeliverWitnessTx(t *testing.T) {
 
 	res := app.DeliverTx(buf)
 
-	assert.EqualValues(t, 0, res.Code)
+	log.Printf("res = %+v\n", res)
+	assert.EqualValues(t, 0, res.Code, res.Log)
 }
