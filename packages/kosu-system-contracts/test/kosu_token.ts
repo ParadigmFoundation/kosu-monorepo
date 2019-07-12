ebc0cee86ebb64162cb3732125dc2ba52f4aaaf5
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-07-12 15:35:39 -0500

    Adding actual tests.

--- packages/kosu-system-contracts/test/kosu_token.ts
+++ packages/kosu-system-contracts/test/kosu_token.ts
@@ -1,102 +1,106 @@
+import {ContractArtifact} from "ethereum-types";
 import { fromWei } from "web3-utils";
-describe.only("KosuToken", () => {
-    let token;
 
-    const printInfo = async () => {
-        await token.balanceOf.callAsync(accounts[0]).then(async x => {
-            await web3Wrapper.getBalanceInWeiAsync(token.address).then(b => {
-                const tokens = fromWei(x.toString());
-                const ether = fromWei(b.toString());
-                console.log(`Tokens: ${tokens}, Ether: ${ether}`);
-            });
-        });
-    };
+import {artifacts, KosuTokenContract} from "..";
 
-    before(() => {
-        token = contracts.kosuToken;
+describe("KosuToken", () => {
+    let token, from;
+
+    before(async () => {
+        token = await KosuTokenContract.deployFrom0xArtifactAsync(
+            artifacts.KosuToken as ContractArtifact,
+            provider,
+            txDefaults,
+            "0x0000000000000000000000000000000000000000",
+        );
+        from = accounts[0];
     });
 
-    it("should receive a transfer and mint some tokens", async () => {
-        await printInfo();
-        const resp = await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
-        await web3Wrapper
-            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from: accounts[0] })
-            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
-        await printInfo();
+    describe("bonding", () => {
+            it("should have expected initial output with parameterization", async () => {
 
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther.times(30));
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
-        await token.liquidateTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
-        await printInfo();
+                await web3Wrapper
+                    .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from })
+                    .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
 
-        await token.liquidateTokens.awaitTransactionSuccessAsync(await token.balanceOf.callAsync(accounts[0]));
-        await printInfo();
+                const finalBalance = await token.balanceOf.callAsync(from);
+                const finalSupply = await token.totalSupply.callAsync();
+                const endingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+
+                TestValues.oneEther.times(30).toString().should.eq(finalBalance.toString());
+                TestValues.oneEther.times(30).toString().should.eq(finalSupply.toString());
+                TestValues.oneEther.toString().should.eq(endingEther.toString());
+            });
+
+            describe("fallback", () => {
+                    it("should generate tokens with fallback function", async () => {
+                        const startingBalance = await token.balanceOf.callAsync(from);
+                        const startingSupply = await token.totalSupply.callAsync();
+                        const startingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+                        const estimate = await token.estimateEtherToToken.callAsync(TestValues.oneEther);
+
+                        await web3Wrapper
+                            .sendTransactionAsync({ to: token.address, value: TestValues.oneEther, from })
+                            .then(txHash => web3Wrapper.awaitTransactionSuccessAsync(txHash));
+
+                        const finalBalance = await token.balanceOf.callAsync(from);
+                        const finalSupply = await token.totalSupply.callAsync();
+                        const endingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+
+                        startingBalance.plus(estimate).toString().should.eq(finalBalance.toString());
+                        startingSupply.plus(estimate).toString().should.eq(finalSupply.toString());
+                        startingEther.plus(TestValues.oneEther).toString().should.eq(endingEther.toString());
+                    });
+            });
+
+            describe("bondTokens", () => {
+                it("should generate tokens with fallback function", async () => {
+                    const startingBalance = await token.balanceOf.callAsync(from);
+                    const startingSupply = await token.totalSupply.callAsync();
+                    const startingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+                    const estimate = await token.estimateEtherToToken.callAsync(TestValues.oneEther);
+
+                    token.bondTokens.awaitTransactionSuccessAsync(TestValues.zero, { value: TestValues.oneEther });
+
+                    const finalBalance = await token.balanceOf.callAsync(from);
+                    const finalSupply = await token.totalSupply.callAsync();
+                    const endingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+
+                    startingBalance.plus(estimate).toString().should.eq(finalBalance.toString());
+                    startingSupply.plus(estimate).toString().should.eq(finalSupply.toString());
+                    startingEther.plus(TestValues.oneEther).toString().should.eq(endingEther.toString());
+                });
+            });
+
+            describe("releaseTokens", () => {
+                    it("should sell tokens on the curve", async () => {
+                        const startingBalance = await token.balanceOf.callAsync(from);
+                        const startingSupply = await token.totalSupply.callAsync();
+                        const startingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+                        const estimate = await token.estimateTokenToEther.callAsync(TestValues.oneEther);
+
+                        token.releaseTokens.awaitTransactionSuccessAsync(TestValues.oneEther);
+
+                        const finalBalance = await token.balanceOf.callAsync(from);
+                        const finalSupply = await token.totalSupply.callAsync();
+                        const endingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+
+                        startingBalance.minus(TestValues.oneEther).toString().should.eq(finalBalance.toString(), "balance");
+                        startingSupply.minus(TestValues.oneEther).toString().should.eq(finalSupply.toString(), "supply");
+                        startingEther.minus(estimate).toString().should.eq(endingEther.toString(), "ether");
+                    });
+
+                    it("should empty the contract", async () => {
+                        await token.releaseTokens.awaitTransactionSuccessAsync(await token.balanceOf.callAsync(from));
+
+                        const finalBalance = await token.balanceOf.callAsync(from);
+                        const finalSupply = await token.totalSupply.callAsync();
+                        const endingEther = await web3Wrapper.getBalanceInWeiAsync(token.address);
+
+                        "0".should.eq(finalBalance.toString());
+                        "0".should.eq(finalSupply.toString());
+                        "0".should.eq(endingEther.toString());
+                    });
+            });
     });
 });
