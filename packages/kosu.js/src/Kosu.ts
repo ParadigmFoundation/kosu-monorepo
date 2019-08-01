8c571f767a88999d80da394b773d484e5ca3bff9
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-08-01 09:50:19 -0500

    Fixing error with initializing web3Wrapper.

--- packages/kosu.js/src/Kosu.ts
+++ packages/kosu.js/src/Kosu.ts
@@ -140,7 +140,7 @@ export class Kosu {
     constructor(options: KosuOptions = { provider: "https://ropsten.infura.io" }) {
         // Configuring web3
         this.web3 = new Web3(options.provider);
-        this.web3Wrapper = new Web3Wrapper(this.web3Wrapper.getProvider());
+        this.web3Wrapper = new Web3Wrapper(options.provider);
         options.web3 = this.web3;
         options.web3Wrapper = this.web3Wrapper;
 
