a6579b0ca8e53770f2f3023c76f8f3e636c9d272
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-08-19 12:57:27 -0500

    Updating script to receive the dockerfile

--- packages/kosu-geth/scripts/build-and-push-geth-image.sh
+++ packages/kosu-geth/scripts/build-and-push-geth-image.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 # image name passed in as args (including registry host)
-IMAGE_HOST=$1
-IMAGE_NAME=$2
-IMAGE_VERSION=$3
+DOCKER_FILE=$1
+IMAGE_HOST=$2
+IMAGE_NAME=$3
+IMAGE_VERSION=$4
 
-GCP_BUCKET_URI=$4
+GCP_BUCKET_URI=$5
 
 # full label of geth image to be built
 IMAGE_LABEL_VERSION="$IMAGE_HOST/$IMAGE_NAME:$IMAGE_VERSION"
 IMAGE_LABEL_LATEST="$IMAGE_HOST/$IMAGE_NAME:latest"
 
 # build kosu-geth image, tag as latest and push both to GCR
-docker build -t $IMAGE_LABEL_VERSION .
+docker build -t $IMAGE_LABEL_VERSION . -f DOCKER_FILE
 docker tag $IMAGE_LABEL_VERSION $IMAGE_LABEL_LATEST
 docker push $IMAGE_LABEL_VERSION
 docker push $IMAGE_LABEL_LATEST
