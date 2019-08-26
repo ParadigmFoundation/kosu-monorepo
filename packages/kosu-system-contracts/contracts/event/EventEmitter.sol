d3dee22bb89c07eb123f6e7fa3962ecee3aa2202
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-08-26 12:14:16 -0500

    Fixing a wording thing.

--- packages/kosu-system-contracts/contracts/event/EventEmitter.sol
+++ packages/kosu-system-contracts/contracts/event/EventEmitter.sol
@@ -18,8 +18,8 @@ contract EventEmitter is Authorizable {
     constructor(address auth) Authorizable(auth) public {
     }
 
-    /** @dev Emits a standard event from the Kosu contract system.  The events can be decoded though the javascript library.
-        @notice Emits a standard event from the Kosu contract system.  The events can be decoded though the javascript library.
+    /** @dev Emits a standard event from the Kosu contract system.  The events can be decoded though the Kosu.js library.
+        @notice Emits a standard event from the Kosu contract system.  The events can be decoded though the Kosu.js library.
         @param eventType String name/type of event.
         @param data Bytes32 encoded data to be emitted.
         @param stringData String containing optional additional information.
