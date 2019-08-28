f6b78198d5da89ef3e1533e4217445738b64d7d6
Henry Harder <henry@paradigm.market>
2019-08-28 16:19:25 -0700

    update unused parameters

--- packages/network-explorer/src/fieldDefinitions.ts
+++ packages/network-explorer/src/fieldDefinitions.ts
@@ -1,18 +1,19 @@
 import { BigNumber } from "@0x/utils";
 import { Kosu } from "@kosu/kosu.js";
+
 import { RedisWrapper } from "./RedisWrapper";
 
 export const fields = {
     "token/total_supply": {
         updateEvery: 30000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const val = await kosu.kosuToken.totalSupply();
             return val.toString();
         },
     },
     "token/price": {
         updateEvery: 30000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const one = new BigNumber(kosu.web3.utils.toWei("1"));
             const val = await kosu.kosuToken.estimateTokenToEther(one);
             return val.toString();
@@ -20,14 +21,14 @@ export const fields = {
     },
     "bandwidth/total_limit": {
         updateEvery: 1800000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const roundInfo = await kosu.node.roundInfo();
             return roundInfo.limit.toString();
         },
     },
     "bandwidth/total_orders": {
         updateEvery: 3000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             try {
                 const orders = await kosu.node.totalOrders();
                 return orders.toString();
@@ -38,14 +39,14 @@ export const fields = {
     },
     "bandwidth/remaining_limit": {
         updateEvery: 4000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const limit = await kosu.node.remainingLimit();
             return limit.toString();
         },
     },
     "bandwidth/number_posters": {
         updateEvery: 5000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const posters = await kosu.node.numberPosters();
             return posters.toString();
         },
@@ -75,21 +76,21 @@ export const fields = {
     },
     "bandwidth/rebalance_period_number": {
         updateEvery: 10000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const roundInfo = await kosu.node.roundInfo();
             return roundInfo.number;
         },
     },
     "network/number_validators": {
         updateEvery: 5000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const validators = await kosu.node.validators();
             return validators.length.toString();
         },
     },
     "network/total_validator_stake": {
         updateEvery: 3600000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             let totalStake = new BigNumber(0);
             const validators = await kosu.node.validators();
             for (const validator of validators) {
@@ -100,7 +101,7 @@ export const fields = {
     },
     "network/total_poster_stake": {
         updateEvery: 60000,
-        updateFunc: async (_this, kosu: Kosu, db: RedisWrapper) => {
+        updateFunc: async (_this, kosu: Kosu, _: RedisWrapper) => {
             const raw = await kosu.posterRegistry.tokensContributed();
             return raw.toString();
         },
