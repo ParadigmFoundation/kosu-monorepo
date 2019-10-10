6ac2d5723f5a84cf5141f6b18e3649068b4684da
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-10-10 10:36:33 -0500

    Commiting types in a state I would think would work.

--- packages/kosu-types/src/index.ts
+++ packages/kosu-types/src/index.ts
@@ -1,17 +1,3 @@
-import {
-    AuthorizedAddressesContract,
-    BasicTradeSubContractContract,
-    EventEmitterContract,
-    KosuTokenContract,
-    OrderGatewayContract,
-    PosterRegistryContract,
-    TreasuryContract,
-    ValidatorRegistryContract,
-    VotingContract,
-    ZeroExV2SubContractContract,
-} from "@kosu/system-contracts";
-import { BigNumber } from "bignumber.js";
-
 export interface MigratedContracts {
     orderGateway: OrderGatewayContract;
     eventEmitter: EventEmitterContract;
@@ -63,4 +49,97 @@ export interface PrettyListing {
     rewardRate: string;
     lastRewardBlock: string;
     currentChallenge: string;
+}
+
+interface KosuOptions {
+    provider?: provider;
+    networkId?: number | string;
+    web3?: Web3;
+    web3Wrapper?: Web3Wrapper;
+    kosuNodeUrl?: string;
+    votingAddress?: string;
+    treasuryAddress?: string;
+    kosuTokenAddress?: string;
+    eventEmitterAddress?: string;
+    orderGatewayAddress?: string;
+    posterRegistryAddress?: string;
+    validatorRegistryAddress?: string;
+}
+
+interface Order {
+    subContract: string;
+    maker: string;
+    makerValues: any;
+    takerValues?: any;
+    makerSignature?: any;
+    arguments?: any;
+    id: any;
+    poster: string;
+}
+
+interface PostableOrder extends Order {
+    posterSignature: string;
+}
+
+interface TakeableOrder extends Order {
+    takerValues: any;
+}
+
+interface OrderArgument {
+    name: string;
+    datatype: string;
+}
+
+interface KosuUtils {
+    toBytes32(value: string): number | string | BN;
+    NULL_ADDRESS: string;
+}
+
+interface Signature {
+    generate(web3: Web3, messageHex: string, signer: string): Promise<string>;
+    validate(messageHex: string, signature: string, signer: string): boolean;
+    recoverAddress(messageHex: any, signature: string): string;
+    sign(web3: Web3, messageHex: string, signer: string): Promise<string>;
+}
+
+interface DecodedKosuLogArgs {}
+
+interface LogWithDecodedKosuArgs<A, B> extends LogWithDecodedArgs<A> {
+    event: string;
+    args: A;
+    decodedArgs: B;
+}
+
+interface OrderValidationResult {
+    accepted: string[];
+    rejected: OrderRejectionInfo[];
+}
+
+interface OrderRejectionInfo {
+    order: string;
+    reason: string;
+}
+
+interface Poster {
+    balance: BigNumber;
+    limit: number;
+}
+
+interface Validator {
+    balance: BigNumber;
+    power: number;
+    publicKey: string;
+    firstVote: number;
+    lastVoted: number;
+    lastProposed: number;
+    totalVotes: number;
+    active: number;
+    applied: number;
+}
+
+interface RoundInfo {
+    number: number;
+    startsAt: number;
+    endsAt: number;
+    limit: number;
 }
