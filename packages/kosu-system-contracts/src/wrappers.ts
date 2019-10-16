59b9c231674f1ab4a3e4b8e8011f1bfe8dbd253d
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-10-16 16:18:57 -0500

    Regenerating artifacts and wrappers removing the custom types.  The temporary fix to allow for proper  devdoc output from compiler has be handled within the appropriate 0x typings package.

--- packages/kosu-system-contracts/src/wrappers.ts
+++ packages/kosu-system-contracts/src/wrappers.ts
@@ -4,14 +4,14 @@
  * -----------------------------------------------------------------------------
  */
 export * from "../generated-wrappers/authorized_addresses";
+export * from "../generated-wrappers/basic_trade_sub_contract";
 export * from "../generated-wrappers/event_emitter";
 export * from "../generated-wrappers/kosu_token";
 export * from "../generated-wrappers/order_gateway";
 export * from "../generated-wrappers/poster_registry";
+export * from "../generated-wrappers/signature_validator_sub_contract";
+export * from "../generated-wrappers/test_token";
 export * from "../generated-wrappers/treasury";
 export * from "../generated-wrappers/validator_registry";
 export * from "../generated-wrappers/voting";
 export * from "../generated-wrappers/zero_ex_v2_sub_contract";
-export * from "../generated-wrappers/basic_trade_sub_contract";
-export * from "../generated-wrappers/signature_validator_sub_contract";
-export * from "../generated-wrappers/test_token";
