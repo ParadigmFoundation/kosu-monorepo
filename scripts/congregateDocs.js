e36f09ca44904ff7c2595281b3ce01a668501c9c
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-10-31 13:20:35 -0500

    Running prettier

--- scripts/congregateDocs.js
+++ scripts/congregateDocs.js
@@ -4,19 +4,15 @@
 const fs = require("fs");
 const { spawn } = require("child_process");
 
-fs.readdir('./packages',
-    (err, packages) => {
-        for (const _package of packages) {
-            if ((["kosu-docs", ".DS_Store"]).includes(_package)) {
-                continue;
-            }
-            fs.readdir(`./packages/${_package}`,
-                (err, dirs) => {
-                    if (dirs.includes('docs')) {
-                        spawn("cp", ["-r", `packages/${_package}/docs`, `packages/kosu-docs/docs/${_package}`]);
-                    }
-                }
-            );
+fs.readdir("./packages", (err, packages) => {
+    for (const _package of packages) {
+        if (["kosu-docs", ".DS_Store"].includes(_package)) {
+            continue;
         }
+        fs.readdir(`./packages/${_package}`, (err, dirs) => {
+            if (dirs.includes("docs")) {
+                spawn("cp", ["-r", `packages/${_package}/docs`, `packages/kosu-docs/docs/${_package}`]);
+            }
+        });
     }
-);
+});
