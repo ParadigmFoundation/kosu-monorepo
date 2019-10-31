fe40274702b202a2487d2d2d311f479c04c9c40a
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-10-31 13:11:35 -0500

    Adding concept script

---
+++ scripts/congregateDocs.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require("fs");
+const { spawn } = require("child_process");
+
+fs.readdir('./packages',
+    (err, packages) => {
+        for (const _package of packages) {
+            if ((["kosu-docs", ".DS_Store"]).includes(_package)) {
+                continue;
+            }
+            fs.readdir(`./packages/${_package}`,
+                (err, dirs) => {
+                    if (dirs.includes('docs')) {
+                        spawn("cp", ["-r", `packages/${_package}/docs`, `packages/kosu-docs/docs/${_package}`]);
+                    }
+                }
+            );
+        }
+    }
+);
